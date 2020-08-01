bd7c64610634f5778071fa2f661fb47a8663faac
Matthew Aiello-Lammens <matt.lammens@gmail.com>
2020-07-31 21:38:10 -0400

    Full restructuring of folders and files into R package format

--- R/demoniche_model.R
+++ R/demoniche_model.R
@@ -1,19 +1,30 @@
+#' @title Run spatially explicit demographic model
+#' @export demoniche_model
+#'
+#' @param modelname Name of \code{list} object containing model parameters
+#' @param Niche TRUE/FALSE that specifies whether to run the simulation accounting for effects of Niche values
+#' @param Dispersal TRUE/FALSE that specifies whether to allow long-distance dispersal
+#' @param repetitions Number of replicate simulations to carry out (default = 1000)
+#' @param foldername Path name of folder where results of simulations will be written to.
+#'
+#' @return Something very special
+#'
 demoniche_model <-
   function(modelname,
            Niche,
            Dispersal,
-           repetitions,
+           repetitions = 1000,
            foldername)
   {
     BEMDEM <-
       get(modelname, envir = .GlobalEnv)   #assign("BEMDEM", get(modelname))
-    
+
     #load(paste(modelname, ".rda", sep = ""))
     #  rm(Hmontana)
     # require(sp)
-    require(popbio)
-    require(lattice)
-    
+    #require(popbio)
+    #require(lattice)
+
     #### Create vectors ############################################################
     Projection <- array(
       0,
@@ -34,13 +45,13 @@ demoniche_model <-
           paste(BEMDEM$years_projections)
         )
     )
-    
+
     eigen_results <-
       vector(mode = "list" , length(BEMDEM$list_names_matrices))
     names(eigen_results) <- unlist(BEMDEM$list_names_matrices)
-    
+
     yrs_total <- BEMDEM$no_yrs * length(BEMDEM$years_projections)
-    
+
     population_sizes <- array(
       NA,
       # per matrix!
@@ -51,7 +62,7 @@ demoniche_model <-
         paste("rep", 1:repetitions, sep = "_")
       )
     )
-    
+
     population_results  <- array(
       1:200,
       # for all matrices!
@@ -62,7 +73,7 @@ demoniche_model <-
         paste(BEMDEM$list_names_matrices)
       )
     )
-    
+
     metapop_results <- array(
       NA,
       # per matrix!
@@ -73,7 +84,7 @@ demoniche_model <-
         paste("rep", 1:repetitions, sep = "_")
       )
     )
-    
+
     simulation_results <- array(
       NA,
       dim = c(
@@ -95,7 +106,7 @@ demoniche_model <-
           )
         )
     )
-    
+
     EMA <- array(
       0,
       dim = c(
@@ -112,28 +123,28 @@ demoniche_model <-
           c("EMA", "No_populations")
         )
     )
-    
+
     population_Niche <- rep(1, nrow(BEMDEM$Niche_ID))
-    
+
     simulation_results[, "initial_population_area"] <-
       sum(BEMDEM$Orig_Populations[, "area_population"])
     # original population size
     simulation_results[, "initial_population"] <-
       round(sum(colSums(BEMDEM$n0_all) * BEMDEM$sumweight), 0)
-    
-    
+
+
     dir.create(paste(getwd(), "/", foldername, sep = ""), showWarnings = FALSE)
-    
-    
-    
-    
+
+
+
+
     #### Start repetitions #########################################################
-    
+
     for (rx in 1:repetitions)
       # tx = 1   rx = 1
     {
       print(paste("Starting projections for repetition:", rx), quote = FALSE)
-      
+
       for (mx in 1:length(BEMDEM$list_names_matrices))
         # selects two matrices for the simulations   mx = 1
       {
@@ -141,15 +152,15 @@ demoniche_model <-
           "Projecting for scenario/matrix:",
           (BEMDEM$list_names_matrices)[mx]
         ), quote = FALSE)
-        
+
         yx_tx <- 0 # restart counter
-        
+
         # this selects two matrices, the first basic matrix and the projection one mx = 1
         Matrix_projection <-
           cbind(BEMDEM$matrices[, 1], (BEMDEM$matrices[, mx]))
-        
+
         # this selects two standard deviation matrices, the first basic matrix and the projection one (column mx)
-        
+
         if (BEMDEM$matrices_var[1] != FALSE)
           # If matrices are included or not.
         {
@@ -164,9 +175,9 @@ demoniche_model <-
         {
           Matrix_projection_var <- FALSE
         }
-        
+
         prev_mx <- rep(1, times = yrs_total + 1)
-        
+
         for (tx in 1:length(BEMDEM$years_projections))
           # selects which time-slice of the simulation tx = 1
         {
@@ -178,10 +189,10 @@ demoniche_model <-
           for (yx in 1:BEMDEM$no_yrs)
           {
             yx_tx <- yx_tx + 1 # add one year to counter
-            
+
             ####################################### PATCH PROJECTION when tx = 1 ###########     tx=1  px=1   tx = 2
             ################################################################################     yx=2  yx = 1
-            
+
             if (tx == 1 && yx == 1)
             {
               n0s <-
@@ -193,7 +204,7 @@ demoniche_model <-
               if (tx != 1 &&
                   yx == 1) {
                 # when going to next time step, yx == 1
-                
+
                 n0s <-
                   t(Projection[BEMDEM$no_yrs, , colSums(Projection[BEMDEM$no_yrs, , , tx -
                                                                      1]) > 0, tx - 1])
@@ -201,34 +212,34 @@ demoniche_model <-
                   which(colSums(Projection[BEMDEM$no_yrs, , , tx - 1]) > 0)
                 BEMDEM$Niche_ID[colSums(Projection[BEMDEM$no_yrs, , , tx -
                                                      1]) > 0, 2]
-                
+
               } else  {
                 # take population from previous year, same time period
                 n0s <-
                   t(Projection[yx - 1, , colSums(Projection[yx - 1, , , tx]) > 0, tx])
                 n0s_ID <-
                   which(colSums(Projection[yx - 1, , , tx]) > 0)
-                
+
               }
-            } 
+            }
             # n <- c(10,5,5,5,5,5)
-            
+
             population_Niche_short <-
               population_Niche[n0s_ID]
-            
+
             # run fcn for each population px=1
             if (nrow(n0s) > 0) {
               for (px in 1:nrow(n0s))  {
                 # px = 1 tx = 1
                 n <- as.vector(n0s[px, ])
-                
+
                 #  source('demoniche_population.r')
                 # if (sum(n) > 0) {
-                
+
                 # selects the right K for this pop and timeperiod
                 populationmax <-
                   BEMDEM$populationmax_all[n0s_ID[px], tx]
-                
+
                 Projection[yx, , n0s_ID[px], tx]    <-
                   demoniche_population(
                     Matrix_projection = Matrix_projection,
@@ -247,18 +258,18 @@ demoniche_model <-
                     env_stochas_type = BEMDEM$env_stochas_type,
                     yx_tx = yx_tx
                   )
-                
+
                 #     } # end if n > 0
               } # end if n0s > 0
             } # end px for
-            
+
             #  Which many patches persist since last timestep (including seeds)?
             metapop_results[yx_tx, mx, rx] <-
               length(intersect(which(colSums(
                 Projection[yx, , , tx]
               ) > 1), n0s_ID))
             ################################################################################
-            
+
             ##################### DISPERSAL FOR ALL PATCHES ####################
             # Calculate dispersal for one repetition (rx), one matrix (mx), one time period(tx), and one year
             # and all populationes.
@@ -272,7 +283,7 @@ demoniche_model <-
                 if (Niche == TRUE) {
                   population_Niche <- BEMDEM$Niche_values[, tx]
                 }
-                
+
                 # source("demoniche_dispersal.r")
                 disp <-
                   demoniche_dispersal(
@@ -282,31 +293,31 @@ demoniche_model <-
                     dist_latlong = BEMDEM$dist_latlong,
                     neigh_index = BEMDEM$neigh_index,
                     fraction_SDD = BEMDEM$fraction_SDD
-                  ) 
-                
+                  )
+
                 Projection[yx, 1, , tx] <- disp
-                
+
               } # end Dispersal if
             } # end sum if
             ################################################################################
-            
+
             # save the results from one run
             population_sizes[yx_tx, mx, rx] <-
               sum(rowSums(Projection[yx, , , tx]) * BEMDEM$sumweight)
-            
+
           } # end yx
-          
+
           EMA[rx, mx, tx, 1] <- # the minimum population each repetition
             min(apply((Projection[, , , tx] * BEMDEM$sumweight), 1, sum))
-          
+
           EMA[rx, mx, tx, 2] <-
             # the number of populations that exist each repetition
             sum(colSums(Projection[yx, , , tx]) > 1)
-          
+
           simulation_results[mx, 7 + tx] <- mean(EMA[, mx, tx, 1])
-          
+
         } # end tx loop
-        
+
         save(
           Projection,
           file = paste(
@@ -321,7 +332,7 @@ demoniche_model <-
             sep = ""
           )
         )
-        
+
         save(
           population_sizes,
           file = paste(
@@ -333,17 +344,17 @@ demoniche_model <-
             sep = ""
           )
         )
-        
+
         # PLOTS of spatial occupancy from the last repetition  yx = 1
         pop <- data.frame(cbind(BEMDEM$Niche_ID[, 2:3],
                                 (
                                   colSums(Projection[yx, , , ] * BEMDEM$sumweight)
                                 )))
-        
+
         form <-
           as.formula(paste(paste(colnames(pop)[-c(1:2)], collapse = "+"), "X+Y", sep =
                              "~"))
-        
+
         jpeg(
           filename = paste(
             getwd(),
@@ -355,7 +366,7 @@ demoniche_model <-
             sep = ""
           )
         )
-        print(levelplot(
+        print(lattice::levelplot(
           form,
           pop,
           col.regions = rev(heat.colors(100)),
@@ -368,32 +379,32 @@ demoniche_model <-
           )
         ))
         dev.off()
-    
-        
+
+
       } # end mx loop
-      
+
     } # end rx loop
-    
+
     rm(Projection)
-    
+
     #### END OF MODELLING #################################################
     ####################################################################################
-    
+
     # extra loop to calculate mean, eigenvalues, etc
     print("Calculating summary values", quote = FALSE)
-    
+
     for (mx in 1:length(BEMDEM$list_names_matrices))
       # mx = 1
     {
       # for 'eigen_results' object for each matrix.
       eigen_results[[mx]] <-
-        c(eigen.analysis(matrix(
+        c(popbio::eigen.analysis(matrix(
           BEMDEM$matrices[, mx],
           ncol = length(BEMDEM$stages),
           byrow = FALSE
         )),
         LTRE = list(matrix(
-          LTRE(
+          popbio::LTRE(
             matrix(
               BEMDEM$matrices[, mx],
               ncol = length(BEMDEM$stages),
@@ -407,12 +418,12 @@ demoniche_model <-
           ),
           nrow = length(BEMDEM$stages)
         )))
-      
+
       simulation_results[mx, "lambda"] <-
         eigen_results[[mx]]$lambda1
-      
+
       simulation_results[mx, "stoch_lambda"] <-
-        stoch.growth.rate(
+        popbio::stoch.growth.rate(
           list(
             matrix(BEMDEM$matrices[, 1],
                    ncol = length(BEMDEM$stages)),
@@ -423,18 +434,18 @@ demoniche_model <-
           maxt = 50000,
           verbose = FALSE
         )$sim
-      
+
       simulation_results[mx, "mean_final_pop"] <-
         mean(population_sizes[yrs_total, mx, ])
-      
+
       # Final mean percentage of patches extinct
       #   simulation_results[mx, "mean_perc_ext_final"] <- NA
       #                      (metapop_results[1,mx,] - mean(metapop_results[yrs_total,mx,]))/metapop_results[1,mx,]*100
-      
+
       simulation_results[mx, "mean_no_patches_final"] <-
         mean(EMA[, mx, length(BEMDEM$years_projections), 2])
-      
-      
+
+
       for (yx_tx in 1:yrs_total)
       {
         # mean of all repetitions
@@ -450,16 +461,16 @@ demoniche_model <-
         population_results[yx_tx, "Max", mx] <-
           max(population_sizes[yx_tx, mx, ])
       }
-      
+
       #  jpeg(file = paste(getwd(), "/", foldername, "/mean_",
       #       BEMDEM$list_names_matrices[mx], ".jpeg", sep = ""))
       #   plot(population_results[,"Meanpop",mx], ylim = c(0, max(population_results[,"Meanpop",mx])),
       #       main = paste("meanpopulation", BEMDEM$list_names_matrices[mx], foldername, sep = "_"),type = "l",
       #        xlab = "Year", ylab = "Population")
       #   dev.off()
-      
+
     } # end mx loop number 2, for eigenvalues.
-    
+
     ######## PLOTS ################
     # plot EMAS
     jpeg(
@@ -487,7 +498,7 @@ demoniche_model <-
     axis(1,
          at = 1:length(BEMDEM$years_projections),
          labels = FALSE)
-    
+
     text(
       1:length(BEMDEM$years_projections),
       y = par("usr")[3] - 5,
@@ -505,7 +516,7 @@ demoniche_model <-
           line = 6,
           cex = 1.7)
     axis(2, xpd = TRUE, las = 1)
-    
+
     legend(
       "topright",
       legend = BEMDEM$list_names_matrices,
@@ -513,9 +524,9 @@ demoniche_model <-
       fill = 1:mx
     )
     title("EMA for different matrices", cex = 0.9)
-    
+
     dev.off()
-    
+
     #### plot population results
     jpeg(
       filename = paste(getwd(), "/", foldername, "/population_results.jpeg", sep = ""),
@@ -567,7 +578,7 @@ demoniche_model <-
     )
     title("Population sizes (+- 1 SD) for different scenarios")
     dev.off()
-    
+
     #########################################################
     write.table(
       simulation_results,
@@ -576,32 +587,32 @@ demoniche_model <-
         paste(getwd(), "/", foldername, "/population_results.csv", sep = ""),
       col.names = NA
     )
-    
+
     save(
       simulation_results,
       file =
         paste(getwd(), "/", foldername, "/population_sizes.rda", sep = "")
     )
-    
+
     save(metapop_results,
          file =
            paste(getwd(), "/", foldername, "/metapop_results.rda", sep = ""))
-    
+
     save(eigen_results,
          file =
            paste(getwd(), "/", foldername, "/eigen_results.rda", sep = ""))
-    
+
     save(EMA, file =
            paste(getwd(), "/", foldername, "/EMA.rda", sep = ""))
-    
+
     save(
       population_results,
       file = paste(getwd(), "/",
                    foldername, "/population_results.rda", sep = "")
     )
-    
+
     print("    All repetitions completed!", quote = FALSE)
-    
+
     return(population_results)
-    
+
   }     # end fcn
